534fcb38afdf1a6804b94aadff4309803d781889
SysgenDev <shlee@sysgen.kr>
2021-01-14 15:26:13 +0900

    Initial Commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public GameObject cube;
+    public TextMeshProUGUI txtScreenSize;
+    public Vector3 RotateAmount;
+    
+    void Update()
+    {
+        cube.transform.Rotate(RotateAmount * Time.deltaTime);
+        txtScreenSize.SetText($"{Screen.width} X {Screen.height}");
+    }
+}
